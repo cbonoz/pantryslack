d5bad1c6578e62fc0ba14ec09dd6097d6c243c53
cbonoz <chrisdistrict@gmail.com>
2017-11-08 23:24:10 -0500

    add shell code for kinesis client library

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
+import {S3} from 'aws-sdk'
+import {AbstractConsumer} from 'kinesis-client-library'
+
 const app = express();
 
 // Custom libraries.
@@ -10,6 +13,56 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+// AWS config skipped for brevity
+const s3 = new S3()
+
+let newlineBuffer = new Buffer('\n')
+
+AbstractConsumer.extend({
+  // create places to hold some data about the consumer
+  initialize(done) {
+    this.cachedRecords = []
+    this.cachedRecordsSize = 0
+    // This MUST be called or processing will never start
+    // That is really really really bad
+    done()
+  },
+
+  processRecords(records, done) {
+    // Put each record into our list of cached records (separated by newlines) and update the size
+    records.forEach(record => {
+      this.cachedRecords.push(record.Data)
+      this.cachedRecords.push(newlineBuffer)
+      this.cachedRecordsSize += (record.Data.length + newlineBuffer.length)
+    })
+
+    // not very good for performance
+    let shouldCheckpoint = this.cachedRecordsSize > 50000000
+
+    // Get more records, but not save a checkpoint
+    if (! shouldCheckpoint) return done()
+
+    return;
+    // TODO (if desired): Upload the records to S3
+    s3.putObject({
+      Bucket: 'my-bucket-name',
+      Key: 'path/to/records/' + Date.now(),
+      Body: Buffer.concat(this.cachedRecords)
+    }, err =>  {
+      if (err) return done(err)
+
+      this.cachedRecords = []
+      this.cachedRecordsSize = 0
+
+      // Pass `true` to checkpoint the latest record we've received
+      done(null, true)
+    })
+  }
+})
+
+
+/* Start the Slack Server */
 
 const server = app.listen(5000, () => {
     console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
